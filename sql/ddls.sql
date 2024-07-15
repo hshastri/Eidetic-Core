3636ec00c72bef52919bfde61893297c74534d17
bananasoup12 <capbrady92@gmail.com>
2024-07-15 12:33:29 -0400

    Adding DDLs and example queries

--- sql/ddls.sql
+++ sql/ddls.sql
@@ -4,3 +4,12 @@ create table percentile_activations(
 );
 
 CREATE INDEX node_idx ON percentile_activations (node_id);
+
+
+create table percentile_distribution(
+	node_id		int not null,
+	threshold_1	double precision not null,
+	threshold_2 double precision not null
+);
+
+CREATE INDEX node_idx_dist ON percentile_distribution (node_id);
--- sql/example_queries.sql
+++ sql/example_queries.sql
@@ -3,3 +3,15 @@ select
 	percentile_disc(0.2) within group (order by percentile_activations.activation)
 from percentile_activations
 	group by node_id;
+
+
+-- insert into percentile_activations(node_id, activation)
+-- values
+-- (1, 10.5),
+-- (1, 11),
+-- (1, 12),
+-- (2, 5),
+-- (2, 6),
+-- (2, 7);
+
+select * from percentile_distribution;
